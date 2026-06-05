b18027d14aeb1121f591da08b318b4f0896d24e0
kordoulakis <ikordoulas@outlook.com>
2026-06-05 13:06:27 +0200

    update project configuration for Cloudflare Workers and refine TypeScript definitions

--- src/app.d.ts
+++ src/app.d.ts
@@ -6,15 +6,14 @@ declare global {
 		// interface Locals {}
 		// interface PageData {}
 		// interface PageState {}
-		// interface Platform {}
 		interface Platform {
-			env: {
-				COUNTER: DurableObjectNamespace;
-			};
-			context: {
-				waitUntil(promise: Promise<any>): void;
-			};
-			caches: CacheStorage & { default: Cache }
+			env: { COUNTER: DurableObjectNamespace };
+			context: { waitUntil(promise: Promise<any>): void };
+			caches: CacheStorage & { default: Cache };
+			env: Env;
+			ctx: ExecutionContext;
+			caches: CacheStorage;
+			cf?: IncomingRequestCfProperties
 		}
 	}
 }
